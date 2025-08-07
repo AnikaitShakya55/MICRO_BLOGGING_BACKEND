d09b3acd0f4bf1c2f13ab898c6c6d0da463e02f9
Anikait Shakya <anikait.shakya@easexpense.com>
2025-08-07 16:05:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const databaseConnection = require("./config/databaseConnection.js");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// middlewares
+app.use(
+  cors({
+    origin: process.env.REACT_APP_FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// api's
+app.use("/api", require("./routes/blogRoutes.js"));
+app.use("/api", require("./routes/userRoutes.js"));
+
+(async () => {
+  databaseConnection();
+
+  app.listen(process.env.BACKEND_PORT, () => {
+    console.log("Server is listening at port 5000");
+  });
+})();
